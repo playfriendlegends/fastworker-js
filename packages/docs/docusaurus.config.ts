9b4a55ea2f873563f45949280c89779a9cf39343
devtend <devtend@friendlegends.com>
2026-05-24 22:20:46 +0700

    chore: configure Docusaurus url and baseUrl for fastworker.dev custom domain

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://playfriendlegends.github.io',
+  url: 'https://fastworker.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/fastworker-js/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
